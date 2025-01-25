599ae0289592c1aa71ba2c0e16f6f2b994a97d0b
satish8454 <satishadabala24@gmail.com>
2025-01-25 21:39:33 +0530

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5=jst@%mdxfw5o#h#-qug)#c)06h4nl0ae)1q7=u0lv)536^_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["thecodersatish.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
