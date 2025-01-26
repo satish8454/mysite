23c43b85f106d01c28e3cfbcc7552bfeaf4df101
satish8454 <satishadabala24@gmail.com>
2025-01-26 21:15:42 +0530

    .

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/",views.login_view,name="login"),
+    path("accounts/login/",views.login_view,name="login"),
     path("signup/",views.signup_view,name="signup"),
     path("logout/",views.logout_view,name='logout'),
     path("chats/",views.chat_view,name='chats'),
